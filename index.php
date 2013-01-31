d74c079f3dcb63950bcb76be7840ddab43b405eb
Kevin Carruthers <kevin.j.carruthers@gmail.com>
2013-01-31 17:43:59 -0500

    Changed link order

--- index.php
+++ index.php
@@ -21,11 +21,13 @@ include_once('nav.inc.php');
 		<a href='http://ca.linkedin.com/in/kevinkarruthers' target='_blank'>LinkedIn</a> | 
 		<a href='mailto:kevin.j.carruthers@gmail.com' target='_blank'>Email</a> 
 		(<a href='media/pgpkey.asc'>PGP</a>)</div>
+<!--
 	<div id='twitterfeed'></div>
+-->
 </content>
 
 </body>
 <!--
-<script type="text/javascript">(function(){var ticker=document.createElement('script');ticker.type='text/javascript';ticker.async=true;ticker.src='//twitcker.com/ticker/KevinKarruthers.js?speed=3&count=25&background=ffffff&open=true&container=own-container&own-container=twitterfeed';(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(ticker);})();</script>
+<script type="text/javascript">(function(){var ticker=document.createElement('script');ticker.type='text/javascript';ticker.async=true;ticker.src='//twitcker.com/ticker/KevinKarruthers.js?speed=2&count=50&background=ffffff&open=true&container=own-container&own-container=twitterfeed';(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(ticker);})();</script>
 -->
 </html>

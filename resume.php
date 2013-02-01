699cd6696dd698da1f6049bc271c4f6b8ca5c070
Kevin Carruthers <kevin.j.carruthers@gmail.com>
2013-02-01 18:31:52 -0500

    Changed resume location

--- resume.php
+++ resume.php
@@ -12,8 +12,8 @@ include_once('nav.inc.php');
 ?>
 
 <content>
-	<p><object data='media/resumecv.pdf' type='application/pdf' class='framed'><iframe src='http://docs.google.com/gview?url=http://pleb.x10.mx/media/resumecv.pdf&embedded=true' class='framed' frameborder='0'></iframe></object><br />
-	<p><a href='media/resumecv.pdf' title='Download'>PDF Download</a>
+	<p><object data='media/resume.pdf' type='application/pdf' class='framed'><iframe src='http://docs.google.com/gview?url=http://pleb.x10.mx/media/resumecv.pdf&embedded=true' class='framed' frameborder='0'></iframe></object><br />
+	<p><a href='media/resume.pdf' title='Download'>PDF Download</a>
 </content>
 
 </body>

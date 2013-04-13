c19c311c258bbab388dbba764f0eec7dbf0d2256
Kevin Carruthers <kevin.j.carruthers@gmail.com>
2013-04-13 05:39:47 -0400

    Complete redo, first iteration of blogging website, initial content

--- index.php
+++ index.php
@@ -2,53 +2,59 @@
 <html lang='en'>
 <head>
 	<title>Kevin Carruthers</title>
-	<link rel='stylesheet' href='style.css'>
-	<link rel='icon' href='favicon.png' />
-</head>
-<body>
-<div id='fade' class='lightboxbg' onclick='document.getElementById("fade").style.display="none";document.getElementById("liplight").style.display="none";document.getElementById("schoollight").style.display="none"'></div>
 
-<content>
-	<div class='wordcloud'>
-		<div class='firstname'>Kevin</div>
-		<div class='lastname'>Carruthers</div>
+<?php
+include_once('nav.inc.php');
+?>
 
-		<div class='symbol'><img src='media/symbol.png' /></div>
-
-		<div class='resume'><a href='media/resume.pdf' target='_blank'>R&#xE9;sum&#xE9;</a></div>
-
-		<div class='github'><a href='https://github.com/KevinKarruthers' target='_blank'>GitHub</a></div>
-		<div class='linkedin'><a href='http://ca.linkedin.com/in/kevinkarruthers' target='_blank'>LinkedIn</a></div>
-		<div class='twitter'><a href='https://twitter.com/KevinKarruthers' target='_blank'>Twitter</a></div>
-		<div class='facebook'><a href='https://www.facebook.com/kevin.j.carruthers' target='_blank'>Facebook</a></div>
-		<div class='email'><a href='mailto:kevin.j.carruthers@gmail.com' target='_blank'>Email</a></div>
-
-		<div class='coursechart'><a href='media/coursechart.png' target='_blank'>SE Course Chart</a></div>
-		<div class='notes'><a href='notes.php'>Notes</a></div>
-		<div class='progress'><a href='progress.php'>SE Progress</a></div>
-
-		<div class='uwhub'><a href='https://github.com/CromiumApps/uwhub' target='_blank'>UWHub</a></div>
-		<div class='se2017'><a href='http://www.se2017.com/' target='_blank'>SE 2017</a></div>
-		<div class='win8homepage'><a href='homepage.php'>Win8Homepage</a></div>
-
-		<div class='lip'><a href='javascript:void(0)' onclick='document.getElementById("liplight").style.display="block";document.getElementById("fade").style.display="block"'>Live! in Print</a></div>
-		<div class='ibid'><a href='media/ibid.pdf' target='_blank'>ibid.</a></div>
-
-		<div class='french'>French</div>
-		<div class='spanish'>Spanish</div>
-		<div class='writing'>Writing</div>
-		<div class='coding'>Coding</div>
-		<div class='music'>Music</div>
-
-
-		<div id='liplight' class='lightbox'>
-			<a href='media/lip1.png' target='_blank'>Issue 1</a><br />
-			<a href='media/lip2.pdf' target='_blank'>Issue 2</a><br />
-			<a href='media/lip5.pdf' target='_blank'>Issue 5</a><br />
-			<a href='media/lip6.pdf' target='_blank'>Issue 6</a>
+<div id='content'>
+	<article>
+		<div id='post2'>
+			<div class='meta'>
+				<div class='cat'>notes, se</div>
+				<div class='date'>April 13th, 2013</div>
+			</div>
+			<div class='mainpost'>
+				<div class='title'>
+					<h1><a href='post2.php'>Notes, Notes, and More Notes</a></h1>
+				</div>
+				<div class='thumb'>
+					<a href='post2.php'>
+						<img src='media/posts/latexvsword.gif' />
+					</a>
+				</div>
+				<div class='excerpt'>
+					<p>I've been told I take good notes. Now, personally, I think that's a gross overstatement, but a million adoring fans can't be wrong (not that I'd know... someone who has a million adoring fans, please let me know if I'm correct).
+					<p>Anyway, people probably like my notes mostly because they're written in LaTeX, and are all formatted and pretty. At least, that's probably why <a href='http://clarisseschneider.tumblr.com'>Clarisse</a> likes them...
+					<p style='float:right;'><a href='post2.php'>continue reading</a>
+				</div>
+			</div>
 		</div>
-	</div>
-</content>
+	</article>
+
+	<article>
+		<div id='post1'>
+			<div class='meta'>
+				<div class='cat'>test, selfie</div>
+				<div class='date'>April 13th, 2013</div>
+			</div>
+			<div class='mainpost'>
+				<div class='title'>
+					<h1><a href='post1.php'>Yet Another New Site Format</a></h1>
+				</div>
+				<div class='thumb'>
+					<a href='post1.php'>
+						<img src='media/posts/newandimproved.jpg' />
+					</a>
+				</div>
+				<div class='excerpt'>
+					<p>Well, I've gone and done it again: I got bored of the most recent layout for my website and went with a completely new one, redesigned from scratch. This time, I've gone with a more bloggy-type of website, which should (hopefully!) prompt me to get back into writing regularly. Or, it'll be a complete...
+					<p style='float:right;'><a href='post1.php'>continue reading</a>
+				</div>
+			</div>
+		</div>
+	</article>
+</div>
 
 </body>
 </html>

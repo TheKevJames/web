38d1f2b534b82be833ec68376e432e865602f2c1
Kevin James <KevinJames@thekev.in>
2018-06-12 13:58:46 -0700

    fix(octodash): review requests moved from "TODO"->"To Review" category

--- octodash/src/components/review.js
+++ octodash/src/components/review.js
@@ -7,6 +7,8 @@ class Review extends Base {
     // TODO: what we really want here is a different list: users/orgs to watch for
     this.queries = accounts.map(account => `is:open is:pr archived:false -assignee:${account.name} -author:${account.name} user:${account.name}&access_token=${account.token}`);
     this.queries = this.queries.concat(accounts.map(account => `is:open is:pr archived:false -assignee:${account.name} -author:${account.name} mentions:${account.name}&access_token=${account.token}`));
+    this.queries = this.queries.concat(accounts.map(account => `is:open is:pr archived:false assignee:${account.name}&access_token=${account.token}`));
+    this.queries = this.queries.concat(accounts.map(account => `is:open is:pr archived:false review-requested:${account.name}&access_token=${account.token}`));
   }
 }
 

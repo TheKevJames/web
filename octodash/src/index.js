46dbd0ab8400cd30e7a219ed316a35892e30a9f7
Kevin James <KevinJames@thekev.in>
2020-03-24 21:52:08 -0700

    chore(octodash): deprecate

--- octodash/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './style/index.css';
-
-import App from './app';
-
-ReactDOM.render(<App />, document.getElementById('root'));
